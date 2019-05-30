8d866398bc55b6f412e0f6a75e489cf1def065a0
Administrator <Administrator@tarena>
2019-05-30 14:53:34 +0800

    hh22

--- test1/src/test1/test1.java
+++ test1/src/test1/test1.java
@@ -3,5 +3,7 @@ package test1;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
 }
